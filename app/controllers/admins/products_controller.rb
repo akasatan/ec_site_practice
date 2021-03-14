34e7dff992ce911951ab2972958dff7be495ca1e
EC2 Default User <ec2-user@ip-172-31-76-84.ec2.internal>
2021-03-14 06:08:35 +0000

    [srart]admin.product.new

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -1,4 +1,23 @@
 class Admins::ProductsController < ApplicationController
   def index
+    @products = Product.all
+  end
+    
+  def new
+    @product = Product.new
+  end
+  
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admins_products_path
+    else
+      root_path
+    end
+  end
+  
+  private
+  def product_params
+    params.require(:product).premit(:name, :description, :price, :product_image, :is_valid)
   end
 end
